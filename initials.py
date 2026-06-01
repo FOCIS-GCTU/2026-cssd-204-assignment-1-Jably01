0eadc08318507db4aa31eb6b5d2f485cdd1c78cd
Jably01 <2425403352@live.gctu.edu.gh>
2026-06-01 10:27:48 -0700

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,3 +1,16 @@
+# File: initials.py
+# Description: Prints  out my initials in large block letters.
+# Assignment Number: 1
+#
+# Name: JABLY JOEL MAWUNYO KOBLA
+# STUDENT ID:  2425403352
+# Email: 2425403352@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Jably Joel Mawunyo Kobla, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
 print("JJJJJJJ   M     M   K    K   JJJJJJJ")
 print("   J      MM   MM   K   K       J   ")
 print("   J      M M M M   K  K        J   ")
